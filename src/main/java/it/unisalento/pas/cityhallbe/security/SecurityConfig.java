675c4cd8785cd0bd03cea15a7a14bb5aa22692ce
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-08-11 18:04:38 +0200

    tentativo security config

--- src/main/java/it/unisalento/pas/cityhallbe/security/SecurityConfig.java
+++ src/main/java/it/unisalento/pas/cityhallbe/security/SecurityConfig.java
@@ -2,44 +2,38 @@ package it.unisalento.pas.cityhallbe.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final JwtTokenConverter jwtTokenConverter = new JwtTokenConverter();
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin((form) -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
-
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.GET, "/api/user/createUser").hasRole("ADMIN")
+                        .anyRequest().denyAll())
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(jwtTokenConverter)
+                ))
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
-
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    public JwtDecoder jwtDecoder() {
+        String issuerUri = "https://smart-city-waste-management.eu.auth0.com/.well-known/jwks.json";
 
-        return new InMemoryUserDetailsManager(user);
+        return NimbusJwtDecoder.withJwkSetUri(issuerUri).build();
     }
 }
