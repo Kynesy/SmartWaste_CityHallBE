1fd729528cda12cae5cf9bc53319e947feec66ec
Cesare <100310104+Kynesy@users.noreply.github.com>
2023-12-14 19:26:17 +0100

    aggiunto design pattern iterator

--- src/main/java/it/unisalento/pas/cityhallbe/services/UserService.java
+++ src/main/java/it/unisalento/pas/cityhallbe/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -116,7 +117,10 @@ public class UserService implements IUserService {
         List<User> userList = userRepository.findAll();
         List<String> idList = new ArrayList<>();
 
-        for (User user : userList) {
+        Iterator<User> iterator = userList.iterator();
+
+        while(iterator.hasNext()){
+            User user = iterator.next();
             idList.add(user.getId());
         }
 
